2672f45b4d13b6d1b4cad967c2ccda9c1dd1d2cc
gr8kev <82469036+gr8kev@users.noreply.github.com>
2025-02-04 14:39:51 +0100

    portfolio

---
+++ app/component/project.js
@@ -0,0 +1,59 @@
+/** @format */
+
+const project = [
+  {
+    name: "DivineLearningHub",
+    description:
+      "DivineLearningHub is an edu firm whose main goal is to provide quality education to students across the globe .",
+    link: "https://www.divinelearninghub.com.ng/",
+    type: "Done + Private",
+    github: "",
+    npm: "",
+  },
+  {
+    name: "CriptonxStudios",
+    description:
+      "Criptonx Studios is an innovative audiovisual production company based in the vibrant heart of Lagos, Africa's largest commercial city, delivering top-tier audio production, videography and photography services. ",
+    link: "https://criptonx.com/",
+    type: "Done + Private",
+    github: "",
+  },
+  {
+    name: "weddingflix",
+    description:
+      "WeddingFlix allows couples to create personalized wedding websites to share event details, manage RSVPs, and provide essential guest information with customizable templates.",
+    link: "https://www.weddingflix.me/",
+    type: "Done + Private",
+    github: "",
+    npm: "",
+  },
+  {
+    name: "Advaaise",
+    description:
+      "Advaatise is a video advertising company  that helps businesses reach their target audience through video content .",
+    link: "https://www.advaatise.com/",
+    type: "Done + Private",
+    github: "",
+    npm: "",
+  },
+  {
+    name: "Oneway Template",
+    description:
+      "OneWayTemplate is a Nigerian platform offering professionally designed templates for Canva, PSD, Figma, XD, Google Docs, and more. Tailored for content creators, marketers, and designers",
+    link: "https://www.onewaytemplate.com/",
+    type: "Done + Private",
+    github: "",
+    npm: "",
+  },
+
+  {
+    name: "FDGS Energy group",
+    description:
+      "FDGS Energy Group is a Nigerian-based energy company specializing in oil trading, maritime logistics, and project management. Established in 2006, it offers reliable services in the global energy sector.",
+    link: "https://www.fdgsenergygroup.ch/",
+    type: "Done + Private",
+    github: "",
+    npm: "",
+  },
+];
+export default project;
